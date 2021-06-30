4cd3c25f22a7c508a23dda785ec774a2e65d3743
송가영 <sgyoung96@naver.com>
2021-06-30 16:55:55 +0900

    한쪽 모서리만 둥근 뷰 만들기 예제- Glide 사용

---
+++ oneCornerRoundView/app/src/main/java/com/example/onecornerroundview/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.onecornerroundview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Glide.with(this).load(getDrawable(R.drawable.group_32)).transform(CenterInside(), GranularRoundedCorners(50F, 0F, 0F, 0F)).into(iv_image)
+
+    }
+}
