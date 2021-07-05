189192f29a2bbae8276c0003365b79a491ecba66
송가영 <sgyoung96@naver.com>
2021-07-05 10:28:04 +0900

    Edit margin, padding practice project, Add CustomKeypad project

--- CustomKeypadPractice/app/src/main/java/com/example/customkeypadpractice/CustomKeyPadActivity.kt
+++ CustomKeypadPractice/app/src/main/java/com/example/customkeypadpractice/CustomKeyPadActivity.kt
@@ -1,4 +1,4 @@
-package com.example.layoutpractice
+package com.example.customkeypadpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- CustomKeypadPractice/app/src/main/java/com/example/customkeypadpractice/NumInputView.kt
+++ CustomKeypadPractice/app/src/main/java/com/example/customkeypadpractice/NumInputView.kt
@@ -1,9 +1,9 @@
-package com.example.layoutpractice
+package com.example.customkeypadpractice
 
 import android.content.Context
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
-
+// 핸드폰 인증번호 입력 팝업
 // ConstraintLayout 에 4가지 constructor 가 있는데, ConstraintLayout 은 생성자를 정의해 줘야 한다.
 class NumInputView: ConstraintLayout, View.OnClickListener {
 
